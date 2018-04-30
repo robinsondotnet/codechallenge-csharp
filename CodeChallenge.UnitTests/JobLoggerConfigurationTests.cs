abb3244abd7a01ec4c07509e2880f44a200ddcd3
Robinson V <robinsondotnet@hotmail.com>
2018-04-29 23:36:25 -0500

    Add JobLoggerConfigurationTests

---
+++ CodeChallenge.UnitTests/JobLoggerConfigurationTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+using CodeChallenge.JobLogger.Core;
+using Xunit;
+
+namespace CodeChallenge.UnitTests
+{
+    public class JobLoggerConfigurationTests
+    {
+        [Fact]
+        public void ShouldSetFalseValue_WhenItsNotConfigured()
+        {
+            var appSettings = new NameValueConfigurationCollection {["JobLogger:LogToFile"] = {Value = "true"}};
+
+            var loggerConfiguration = new JobLoggerConfiguration(appSettings);
+
+            Assert.True(loggerConfiguration.LogToFile); 
+            Assert.False(loggerConfiguration.LogToConsole);
+            Assert.False(loggerConfiguration.LogToDb);
+        }
+
+        [Fact]
+        public void ShouldThrowException_WhenAllValuesAreFalse() => Assert.Throws<ConfigurationErrorsException>(() =>
+        {
+            var _ = new JobLoggerConfiguration(new NameValueConfigurationCollection());
+        });
+    }
+}
